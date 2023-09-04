bf61ee869019e9d850029eb0d4e96828ac1b222f
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-04 18:20:24 +0200

    Sending response from backend to SearchBar comp.

--- src/pages/api/users/get/[slug].js
+++ src/pages/api/users/get/[slug].js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).end();
@@ -13,5 +15,14 @@ export default async function handler(req, res) {
     return res.status(400).send("Invalid expression. Please try again");
   }
 
-  // res.status(201).json(user);
+  axios.get(`https://api.github.com/users/${slug}/repos`).then(
+    (response) => {
+      let user = response.data;
+      res.status(200).json(user);
+    },
+    (error) => {
+      console.log(error);
+      res.status(404).send("No such user found. Please try again");
+    }
+  );
 }
