34c4b838edcd7ea2de14a8912ee1ba03cdb2d6fa
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-05 19:21:58 +0200

    Displaying two most recent forks

--- src/pages/forks/[...slug].js
+++ src/pages/forks/[...slug].js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 import ThemeToggler from "@/components/ui/ThemeToggler";
 import SingleFork from "@/components/forks/SingleFork";
@@ -11,7 +10,8 @@ const forksSubpage = (props) => {
     <section className="min-h-screen bg-slate-50 dark:bg-slate-800 text-slate-900 dark:text-slate-200">
       <ThemeToggler />
       <h1 className="w-[90%] mx-auto text-center text-3xl font-bold py-8 text-slate-900 dark:text-slate-200">
-        Here is a list of forks for repo {props.slug[1]} - user {props.slug[0]}
+        Here is a list of two most recent forks for repo {props.slug[1]} - user{" "}
+        {props.slug[0]}
       </h1>
       {props.forksList.map((fork) => {
         return <SingleFork key={fork.id} fork={fork} />;
@@ -31,18 +31,6 @@ export async function getServerSideProps(context) {
   const user = slug[0];
   const repo = slug[1];
 
-  // let forksList;
-
-  // axios.get(`/api/users/getForks/${user}/${repo}`).then(
-  //   (response) => {
-  //     console.log(response.data);
-  //     forksList = response.data;
-  //   },
-  //   (error) => {
-  //     console.log("errrrrrrrrrr " + error);
-  //   }
-  // );
-
   let url = `http://localhost:3000/api/users/getForks/${user}/${repo}`;
 
   let res = await fetch(url, {
@@ -55,9 +43,22 @@ export async function getServerSideProps(context) {
 
   let forksList = await res.json();
 
+  function compareDates(a, b) {
+    const dateA = new Date(a.updatedAt);
+    const dateB = new Date(b.updatedAt);
+
+    if (dateA > dateB) return -1;
+    if (dateA < dateB) return 1;
+    return 0;
+  }
+
+  const forksSorted = forksList.sort(compareDates);
+
+  const twoMostRecentForks = forksSorted.slice(0, 2);
+
   return {
     props: {
-      forksList: forksList,
+      forksList: twoMostRecentForks,
       slug: slug,
     },
   };
