7a9cd156be7f70b35efff6b27d16d06c815bb890
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-05 13:24:04 +0200

    Fixed issue with theme toggler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
   theme: {
-    darkMode: "class",
     extend: {},
   },
   plugins: [],
