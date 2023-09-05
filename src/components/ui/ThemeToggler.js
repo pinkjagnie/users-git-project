47599a6f463381bd1411095a6998d9cc811d7c9e
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-05 12:36:49 +0200

    Added ThemeToggler comp.

---
+++ src/components/ui/ThemeToggler.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+
+const ThemeToggler = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setIsDarkMode(savedTheme === "dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDarkMode);
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+
+  const toggleThemeHandler = () => {
+    setIsDarkMode((prevState) => !prevState);
+  };
+
+  return (
+    <button
+      onClick={toggleThemeHandler}
+      className={`sticky top-4 left-6 p-2 rounded-full ${
+        isDarkMode ? "bg-gray-800 text-gray-200" : "bg-gray-200 text-gray-800"
+      }`}
+    >
+      {isDarkMode ? <BsFillSunFill size={20} /> : <BsFillMoonFill size={20} />}
+    </button>
+  );
+};
+
+export default ThemeToggler;
